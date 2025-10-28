8b862643bfdc8f7dcebcccc792ec70a0b323dddb
harikrishnan-devdj <harikrishnan@sriramachandra.edu.in>
2025-10-28 08:26:12 +0530

    modify the username

--- src/utils/email.py
+++ src/utils/email.py
@@ -59,7 +59,7 @@ async def send_credentials_email(email_to: str, username: str, password: str, fu
 
                     <!-- CREDENTIALS BOX -->
                     <div class="credentials-box">
-                        <p>{email_to}</p>
+                        <p>{username}</p>
                         <p><strong>Password:</strong>{password}</p>
                     </div>
 
